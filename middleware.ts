b1ad85e6efba6c9d09dffd6b8d292aea25e4b668
Sam <sam@deblock.com>
2022-12-23 11:03:52 +0100

    Add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
   const country = req.geo?.country;
 
-  console.log("country,country", country);
+  console.log("country,country", country, req);
+  return NextResponse.next();
 }
