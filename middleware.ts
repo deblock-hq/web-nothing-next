fa7593288277e3e04d697436ffbe09c359c232dc
Sam <sam@deblock.com>
2022-12-23 13:16:18 +0100

    Add middleware

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const country = req.geo?.country;
   const url = req.nextUrl.clone();
-  url.pathname = "/fr-FR";
+  url.pathname = "/fr-FR/";
 
   console.log("country,country", country, req.nextUrl.pathname);
 
-  if (req.nextUrl.pathname == "/fr-FR") {
+  if (req.nextUrl.pathname == "/fr-FR/") {
     console.log("path2", req.nextUrl.pathname);
 
     return NextResponse.next();
