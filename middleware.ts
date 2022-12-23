f0690b844c7d6648c4b9e2bbb2c8c46c73c1275d
Sam <sam@deblock.com>
2022-12-23 11:51:30 +0100

    Add middleware

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,14 @@ export function middleware(req: NextRequest) {
   const country = req.geo?.country;
 
   console.log("country,country", country);
-  if (country === "FR")
+
+  if (req.nextUrl.pathname === "/fr-FR") {
+    return NextResponse.next();
+  }
+
+  if (country === "FR") {
     return NextResponse.redirect(new URL("/fr-FR", req.url));
+  }
 }
 
 export const config = {
