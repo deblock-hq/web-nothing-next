7ad1d77b960e39260ead91d29a7c3feb39e10204
Sam <sam@deblock.com>
2023-03-06 11:24:39 +0100

    Test without middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,30 @@
-// import { NextRequest, URLPattern } from "next/server";
-// import { NextResponse } from "next/server";
+import { NextRequest, URLPattern } from "next/server";
+import { NextResponse } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//   const country = req.geo?.country;
-//   const url = req.nextUrl.clone();
-//   url.pathname = "/fr-FR";
-//   // console.log("country,country", country, req.nextUrl.pathname);
-//   const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
-//   if (
-//     req.nextUrl.href.includes("/fr-FR") ||
-//     req.nextUrl.href.includes("/en-FR") ||
-//     req.nextUrl.href.includes("/en-GB") ||
-//     req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
-//   ) {
-//     return NextResponse.next();
-//   }
-//   // console.log("test", req.nextUrl.href.split("/"));
-//   if (
-//     country === "FR" &&
-//     !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
-//   ) {
-//     return NextResponse.redirect(url);
-//   }
-//   console.log("URL", url, req);
-// }
+export function middleware(req: NextRequest) {
+  const country = req.geo?.country;
+  const url = req.nextUrl.clone();
+  url.pathname = "/fr-FR";
+  // console.log("country,country", country, req.nextUrl.pathname);
+  // const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
+  // if (
+  //   req.nextUrl.href.includes("/fr-FR") ||
+  //   req.nextUrl.href.includes("/en-FR") ||
+  //   req.nextUrl.href.includes("/en-GB") ||
+  //   req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
+  // ) {
+  //   return NextResponse.next();
+  // }
+  // // console.log("test", req.nextUrl.href.split("/"));
+  // if (
+  //   country === "FR" &&
+  //   !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
+  // ) {
+  //   return NextResponse.redirect(url);
+  // }
+  // console.log("URL", url, req);
+}
 
-// export const config = {
-//   matcher: "/",
-// };
+export const config = {
+  matcher: "/",
+};
