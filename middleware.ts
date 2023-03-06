d34f0aaa9459774e55f120a74384638c0195b616
Sam <sam@deblock.com>
2023-03-06 11:38:35 +0100

    Test without middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   url.pathname = "/fr-FR";
   // console.log("country,country", country, req.nextUrl.pathname);
-  // const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
+  const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
   // if (
   //   req.nextUrl.href.includes("/fr-FR") ||
   //   req.nextUrl.href.includes("/en-FR") ||
@@ -15,14 +15,14 @@ export function middleware(req: NextRequest) {
   // ) {
   //   return NextResponse.next();
   // }
-  // // console.log("test", req.nextUrl.href.split("/"));
-  // if (
-  //   country === "FR" &&
-  //   !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
-  // ) {
-  //   return NextResponse.redirect(url);
-  // }
-  // console.log("URL", url, req);
+  // console.log("test", req.nextUrl.href.split("/"));
+  if (
+    country === "FR" &&
+    !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
+  ) {
+    return NextResponse.redirect(url);
+  }
+  console.log("URL", url, req);
 }
 
 export const config = {
