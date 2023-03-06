ec4305b52467e3d588cbd1abc8037fdddbe26fff
Sam <sam@deblock.com>
2023-03-06 12:17:38 +0100

    Test on middleware

--- middleware.ts
+++ middleware.ts
@@ -6,24 +6,24 @@ const PUBLIC_FILE = /\.(.*)$/;
 export function middleware(req: NextRequest) {
   const country = req.geo?.country;
   const url = req.nextUrl.clone();
-  url.pathname = "/fr-FR";
-  // console.log("country,country", country, req.nextUrl.pathname);
-  const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
-  if (
-    req.nextUrl.href.includes("/fr-FR") ||
-    req.nextUrl.href.includes("/en-FR") ||
-    req.nextUrl.href.includes("/en-GB") ||
-    req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
-  ) {
-    return NextResponse.next();
-  }
-  // console.log("test", req.nextUrl.href.split("/"));
-  if (
-    country === "FR" &&
-    !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
-  ) {
-    return NextResponse.redirect(url);
-  }
+  // url.pathname = "/fr-FR";
+  // // console.log("country,country", country, req.nextUrl.pathname);
+  // const stringCheck = ["/fr-FR", "/en-FR", "/en-GB"];
+  // if (
+  //   req.nextUrl.href.includes("/fr-FR") ||
+  //   req.nextUrl.href.includes("/en-FR") ||
+  //   req.nextUrl.href.includes("/en-GB") ||
+  //   req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
+  // ) {
+  //   return NextResponse.next();
+  // }
+  // // console.log("test", req.nextUrl.href.split("/"));
+  // if (
+  //   country === "FR" &&
+  //   !req.nextUrl.href.split("/").some((el) => stringCheck.includes(el))
+  // ) {
+  //   return NextResponse.redirect(url);
+  // }
 
   // if (req.nextUrl.locale === "default") {
   //   const locale = req.cookies.get("NEXT_LOCALE")?.value || "/";
